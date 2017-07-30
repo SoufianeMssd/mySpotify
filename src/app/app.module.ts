2590714cce456e4782e09cb29904c95959dab371
soufsaid <soufsaid.ok@gmail.com>
2017-07-30 08:59:00 +0100

    first navBar+search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { SearchComponent } from './search/search.component';
+const appRoot: Routes = [
+  {path: '', component: SearchComponent},
+  {path: 'about', component: AboutComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoot)
   ],
   providers: [],
   bootstrap: [AppComponent]
